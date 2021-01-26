e957c4594a7db000e4e2cc1fcb929a2b4f1c1e7a
Garvit Dewan <d.garvit@gmail.com>
2021-01-27 01:45:41 +0530

    Update verify function

--- parallel.cpp
+++ parallel.cpp
@@ -11,8 +11,7 @@
 /*
  * reading a CSC matrix from a coordinate file, stored col-ordered
  */
-bool readSparseMatrix(std::string fName, int &n, int &NNZ, int* &col,//FIXME change col type to size_t
-                int* &row, double* &val){
+bool readSparseMatrix(std::string fName, int &n, int &NNZ, int* &col, int* &row, double* &val){
  /*This function reads the input matrix from "fName" file and
   * allocate memory for matrix A, L and U.
   * - The input file is a coordinate version and e
@@ -195,95 +194,6 @@ bool readRHSMatrix(std::string fName, double* &val, const int size) {
  return true;
 }
 
-bool printLower(std::string fName){
- /*This function reads the input matrix from "fName" file and
-  * allocate memory for matrix A, L and U.
-  * - The input file is a coordinate version and e
-  * ach row of the file shows (col, row, nnz)
-  * - The matrices are zero-indexed
-  */
-
- std::ifstream inFile;
- double tol=0.1;
- size_t n, NNZ;
- inFile.open(fName);
- std::string line,banner, mtx, crd, arith, sym;
- /*  File format:
-  *    %%MatrixMarket matrix coordinate real general/symmetric/...
-  *    % ...
-  *    % (optional comments)
-  *    % ...
-  *    #rows    #non-zero
-  *    Triplet in the rest of lines: row    col    value
-  */
- std::getline(inFile,line);
- //std::cout<<line<<std::endl;
- std::cout<<"%%MatrixMarket matrix coordinate real symmetric" << std::endl;
- for (unsigned i=0; i<line.length(); line[i]=tolower(line[i]),i++);
- std::istringstream iss(line);
- if (!(iss >> banner >> mtx >> crd >> arith >> sym)){
-  std::cout<<"Invalid header (first line does not contain 5 tokens)" << std::endl;
-  return false;
- }
-
- if(banner.compare("%%matrixmarket")) {
-  std::cout<<"Invalid header (first token is not \"%%%%MatrixMarket\")" << std::endl;
-  return false;
- }
- if(mtx.compare("matrix")) {
-  std::cout<<"Not a matrix; this driver cannot handle that." << std::endl;
-  return false;
- }
- if(crd.compare("coordinate")) {
-  std::cout<<"Not in coordinate format; this driver cannot handle that." << std::endl;
-  return false;
- }
- if(arith.compare("real")) {
-  if(!arith.compare("complex")) {
-   std::cout<<"Complex matrix; use zreadMM instead!" << std::endl;
-   return false;
-  }
-  else if(!arith.compare("pattern")) {
-   std::cout<<"Pattern matrix; values are needed!" << std::endl;
-   return false;
-  }
-  else {
-   std::cout<<"Unknown arithmetic" << std::endl;
-   return false;
-  }
- }
- while (!line.compare(0,1,"%"))
- {
-  std::getline(inFile, line);
-  //std::cout<<line<<std::endl;
- }
- std::istringstream issDim(line);
- if (!(issDim >> n >> n >> NNZ)){
-  std::cout<<"The matrix dimension is missing" << std::endl;
-  return false;
- }
- if(n <= 0 || NNZ <= 0)
-  return false;
- //Initializing the result vector
- int y, x, colCnt=0, nnzCnt=0;
- std::string value;
- std::vector<std::pair<std::pair<int, int>, std::string>> contents; // storing row, col and val
- for (int i = 0; i < NNZ; ++i) {
-    inFile>>x;
-    inFile>>y;
-    inFile>>value;
-    if (x >= y) {
-        contents.push_back(std::make_pair(std::make_pair(x, y), value));
-        ++nnzCnt;
-    }
- }
- std::cout << n << " " << n << " " << nnzCnt << std::endl;
- for (auto f : contents) {
-    std::cout << f.first.first << " " << f.first.second << " " << f.second << std::endl;
- }
- return true;
-}
-
 void visit(std::vector<std::set<int>> const &dag, int idx, bool visited[], std::set<int>& reachset) {
     reachset.insert(idx);
     visited[idx] = true;
@@ -332,7 +242,7 @@ std::set<int> makeReachset(std::vector<std::set<int>> const &dag, bool* &visited
     return reachset;
 }
 
-// Verify if L*x = b
+// Verify if L*x = b. In case of descrepancy, compare with the results generated from naive lsolve
 int verify(int* &col, int* &row, double* &val, int n, double* &x, std::string rhsFName) {
     double *res = new double[n]();
 
@@ -343,10 +253,15 @@ int verify(int* &col, int* &row, double* &val, int n, double* &x, std::string rh
         }
     }
 
-    double *x1;
+    double *x1, *x2;
     if (!readRHSMatrix(rhsFName, x1, n)) {
         std::cout << "Error while reading RHS matrix." << std::endl;
         return -1;
+    }
+
+    *x2 = new double[n]();
+    for (int i = 0; i < n; ++i) {
+        x2[i] = x1[i];
     }
 
     int count = 0;
@@ -355,10 +270,15 @@ int verify(int* &col, int* &row, double* &val, int n, double* &x, std::string rh
         if (diff < 0) {
             diff *= -1;
         }
-        if (diff > 0.001) {
-            ++count;
-            std::cout << i << " " << res[i] << " " << x1[i] << std::endl;
+        if (x1[i] != 0) {
+            if (diff > 0.001) {
+                if (res)
+            }
         }
+        // if (diff > 0.001) {
+        //     ++count;
+        //     std::cout << i << " " << res[i] << " " << x1[i] << std::endl;
+        // }
     }
     return count;
 }
@@ -398,20 +318,8 @@ int main(int argc, char *argv[]) {
     bool *visited = new bool[n]();
     std::vector<std::set<int>> dag = makeDAG(col, row, val, n, visited);
     std::set<int> reachset = makeReachset(dag, visited, n, x);
-    
 
-    // for (auto f : reachset) {
-    //     std::cout << f+1 << ": ";
-    //     for (auto j : dag[f]) {
-    //         std::cout << j+1 << " ";
-    //     }
-    //     std::cout << std::endl;
-    // }
-
-    // std::cout << "here";
-    // exit(0);
-
-#if 1
+#if 0
     // Reach set made. Now Solve
     start = std::chrono::system_clock::now();
     for (auto j : reachset) {
@@ -450,7 +358,7 @@ int main(int argc, char *argv[]) {
     }
     //****************************************************************
 #endif
-#if 0
+#if 1
     // Creating levels and tasks
     std::vector<int> indegree(n); // Keep a count of incoming edges to record root nodes
     std::vector< std::vector<int> > levels; // Store levels of parallelism. Tasks inside same level can be performed parallely; each 2 levels need to sync
@@ -515,10 +423,10 @@ int main(int argc, char *argv[]) {
     }
     end = std::chrono::system_clock::now();
     elapsed_seconds = end-start;
-    std::cout << "Parallel lsolve: " << elapsed_seconds.count() << std::endl;
+    // std::cout << "Parallel lsolve: " << elapsed_seconds.count() << std::endl;
+    std::cout << elapsed_seconds.count() << std::endl;
 
     for (int j = 0; j < n; ++j) {
-        // std::cout << f + 1 << ",";
         x1[j] /= val[col[j]];
         int p = col[j] + 1;
         for (; p < col[j+1]; ++p) {
@@ -565,13 +473,4 @@ int main(int argc, char *argv[]) {
     //     std::cout << x[i] << " " << x1[i] << std::endl;
     // }
 #endif
-
-
-    // for (int i = 0; i < levels.size(); ++i) {
-    //     std::cout << i+1 << ": ";
-    //     for (auto j : levels[i]) {
-    //         std::cout << j+1 << ", ";
-    //     }
-    //     std::cout << std::endl;
-    // }
 }
