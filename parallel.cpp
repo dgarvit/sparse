49713ba2cd2c6b3e42e8ebad4e676b18f8950946
Garvit Dewan <d.garvit@gmail.com>
2021-01-27 01:49:06 +0530

    Seperate read matrix into separate file

--- parallel.cpp
+++ parallel.cpp
@@ -7,192 +7,7 @@
 #include <set>
 #include <vector>
 #include <assert.h>
-
-/*
- * reading a CSC matrix from a coordinate file, stored col-ordered
- */
-bool readSparseMatrix(std::string fName, int &n, int &NNZ, int* &col, int* &row, double* &val){
- /*This function reads the input matrix from "fName" file and
-  * allocate memory for matrix A, L and U.
-  * - The input file is a coordinate version and e
-  * ach row of the file shows (col, row, nnz)
-  * - The matrices are zero-indexed
-  */
-
- std::ifstream inFile;
- inFile.open(fName);
- std::string line,banner, mtx, crd, arith, sym;
- /*  File format:
-  *    %%MatrixMarket matrix coordinate real general/symmetric/...
-  *    % ...
-  *    % (optional comments)
-  *    % ...
-  *    #rows    #non-zero
-  *    Triplet in the rest of lines: row    col    value
-  */
- std::getline(inFile,line);
- for (unsigned i=0; i<line.length(); line[i]=tolower(line[i]),i++);
- std::istringstream iss(line);
- if (!(iss >> banner >> mtx >> crd >> arith >> sym)){
-  std::cout<<"Invalid header (first line does not contain 5 tokens)" << std::endl;
-  return false;
- }
-
- if(banner.compare("%%matrixmarket")) {
-  std::cout<<"Invalid header (first token is not \"%%%%MatrixMarket\")" << std::endl;
-  return false;
- }
- if(mtx.compare("matrix")) {
-  std::cout<<"Not a matrix; this driver cannot handle that." << std::endl;
-  return false;
- }
- if(crd.compare("coordinate")) {
-  std::cout<<"Not in coordinate format; this driver cannot handle that." << std::endl;
-  return false;
- }
- if(arith.compare("real")) {
-  if(!arith.compare("complex")) {
-   std::cout<<"Complex matrix; use zreadMM instead!" << std::endl;
-   return false;
-  }
-  else if(!arith.compare("pattern")) {
-   std::cout<<"Pattern matrix; values are needed!" << std::endl;
-   return false;
-  }
-  else {
-   std::cout<<"Unknown arithmetic" << std::endl;
-   return false;
-  }
- }
- while (!line.compare(0,1,"%"))
- {
-  std::getline(inFile, line);
- }
- std::istringstream issDim(line);
- if (!(issDim >> n >> n >> NNZ)){
-  std::cout<<"The matrix dimension is missing" << std::endl;
-  return false;
- }
- if(n <= 0 || NNZ <= 0)
-  return false;
- col = new int[n + 1]();
- // colL = new int[n + 1]; colU = new int[n + 1];
- row = new int[NNZ];
- // rowL = new int[factorSize]; rowU = new int[factorSize];
- val = new double[NNZ];
- // valL = new double[factorSize]; valU = new double[factorSize];
- if(!val || !col || !row)
-  return false;
- //Initializing the result vector
- int y, x, colCnt=0, nnzCnt=0;
- double value;
-
- col[0]=0;
- for (int i = 0; i < NNZ; ++i) {
-    inFile>>x;//zero indexing
-    inFile>>y;
-    inFile>>value;
-    if(y > n || y > x || x > n) {
-        std::cout << x << " " << y << " " << value ;
-        return false;
-    }
-    val[i] = value;
-    row[i] = x - 1;
-    ++col[y];
- }
- for (int i = 2; i <= n; ++i)
-    col[i] += col[i-1];
- return true;
-}
-
-bool readRHSMatrix(std::string fName, double* &val, const int size) {
-
- std::ifstream inFile;
- inFile.open(fName);
- std::string line,banner, mtx, crd, arith, sym;
- /*  File format:
-  *    %%MatrixMarket matrix coordinate real general/symmetric/...
-  *    % ...
-  *    % (optional comments)
-  *    % ...
-  *    #rows    #non-zero
-  *    Triplet in the rest of lines: row    col    value
-  */
- std::getline(inFile,line);
- for (unsigned i=0; i<line.length(); line[i]=tolower(line[i]),i++);
- std::istringstream iss(line);
- if (!(iss >> banner >> mtx >> crd >> arith >> sym)){
-  std::cout<<"Invalid header (first line does not contain 5 tokens)" << std::endl;
-  return false;
- }
-
- if(banner.compare("%%matrixmarket")) {
-  std::cout<<"Invalid header (first token is not \"%%%%MatrixMarket\")" << std::endl;
-  return false;
- }
- if(mtx.compare("matrix")) {
-  std::cout<<"Not a matrix; this driver cannot handle that." << std::endl;
-  return false;
- }
- if(crd.compare("coordinate")) {
-  std::cout<<"Not in coordinate format; this driver cannot handle that." << std::endl;
-  return false;
- }
- if(arith.compare("real")) {
-  if(!arith.compare("complex")) {
-   std::cout<<"Complex matrix; use zreadMM instead!" << std::endl;
-   return false;
-  }
-  else if(!arith.compare("pattern")) {
-   std::cout<<"Pattern matrix; values are needed!" << std::endl;
-   return false;
-  }
-  else {
-   std::cout<<"Unknown arithmetic" << std::endl;
-   return false;
-  }
- }
- while (!line.compare(0,1,"%"))
- {
-  std::getline(inFile, line);
- }
- int n, cols, NNZ;
- std::istringstream issDim(line);
- if (!(issDim >> n >> cols >> NNZ)){
-  std::cout<<"The matrix dimension is missing" << std::endl;
-  return false;
- }
- if(n <= 0 || NNZ <= 0)
-  return false;
- if (n != size) {
-     std::cout << "Dimensions of L and RHS do not match" << std::endl;
-     std::cout << size << " " << n << std::endl;
-     return false;
- }
- if (cols != 1) {
-     std::cout << "RHS matrix has incorrect dimensions" << std::endl;
-     return false;
- }
- val = new double[n];
- // valL = new double[factorSize]; valU = new double[factorSize];
- if(!val)
-  return false;
- //Initializing the result vector
- int y, x;
- double value;
-
- for (int i = 0; i < NNZ; ++i) {
-    inFile>>x;//zero indexing
-    inFile>>y;
-    inFile>>value;
-    if(y > 1 || x > n) {
-        std::cout << x << " " << y << " " << value ;
-        return false;
-    }
-    val[x-1] = value;
- }
- return true;
-}
+#include "readMatrix.h"
 
 void visit(std::vector<std::set<int>> const &dag, int idx, bool visited[], std::set<int>& reachset) {
     reachset.insert(idx);
@@ -259,7 +74,7 @@ int verify(int* &col, int* &row, double* &val, int n, double* &x, std::string rh
         return -1;
     }
 
-    *x2 = new double[n]();
+    x2 = new double[n]();
     for (int i = 0; i < n; ++i) {
         x2[i] = x1[i];
     }
@@ -272,7 +87,7 @@ int verify(int* &col, int* &row, double* &val, int n, double* &x, std::string rh
         }
         if (x1[i] != 0) {
             if (diff > 0.001) {
-                if (res)
+                
             }
         }
         // if (diff > 0.001) {
@@ -426,14 +241,14 @@ int main(int argc, char *argv[]) {
     // std::cout << "Parallel lsolve: " << elapsed_seconds.count() << std::endl;
     std::cout << elapsed_seconds.count() << std::endl;
 
-    for (int j = 0; j < n; ++j) {
-        x1[j] /= val[col[j]];
-        int p = col[j] + 1;
-        for (; p < col[j+1]; ++p) {
-            x1[row[p]] -= val[p] * x1[j];
-        }
-    }
-    std::cout << verify(col, row, val, n, x, rhsFName) << std::endl;
+    // for (int j = 0; j < n; ++j) {
+    //     x1[j] /= val[col[j]];
+    //     int p = col[j] + 1;
+    //     for (; p < col[j+1]; ++p) {
+    //         x1[row[p]] -= val[p] * x1[j];
+    //     }
+    // }
+    // std::cout << verify(col, row, val, n, x, rhsFName) << std::endl;
     // VERIFY
     // double *res = new double[n]();
     // for (int i = 0; i < n; ++i) {
---
+++ readMatrix.h
@@ -0,0 +1,195 @@
+#include <omp.h>
+#include <cstdio>
+#include <fstream>
+#include <iostream>
+#include <chrono>
+#include <sstream>
+#include <set>
+#include <vector>
+#include <assert.h>
+
+/*
+ * reading a CSC matrix from a coordinate file, stored col-ordered
+ */
+bool readSparseMatrix(std::string fName, int &n, int &NNZ, int* &col, int* &row, double* &val){
+ /*This function reads the input matrix from "fName" file and
+  * allocate memory for matrix A, L and U.
+  * - The input file is a coordinate version and e
+  * ach row of the file shows (col, row, nnz)
+  * - The matrices are zero-indexed
+  */
+
+ std::ifstream inFile;
+ inFile.open(fName);
+ std::string line,banner, mtx, crd, arith, sym;
+ /*  File format:
+  *    %%MatrixMarket matrix coordinate real general/symmetric/...
+  *    % ...
+  *    % (optional comments)
+  *    % ...
+  *    #rows    #non-zero
+  *    Triplet in the rest of lines: row    col    value
+  */
+ std::getline(inFile,line);
+ for (unsigned i=0; i<line.length(); line[i]=tolower(line[i]),i++);
+ std::istringstream iss(line);
+ if (!(iss >> banner >> mtx >> crd >> arith >> sym)){
+  std::cout<<"Invalid header (first line does not contain 5 tokens)" << std::endl;
+  return false;
+ }
+
+ if(banner.compare("%%matrixmarket")) {
+  std::cout<<"Invalid header (first token is not \"%%%%MatrixMarket\")" << std::endl;
+  return false;
+ }
+ if(mtx.compare("matrix")) {
+  std::cout<<"Not a matrix; this driver cannot handle that." << std::endl;
+  return false;
+ }
+ if(crd.compare("coordinate")) {
+  std::cout<<"Not in coordinate format; this driver cannot handle that." << std::endl;
+  return false;
+ }
+ if(arith.compare("real")) {
+  if(!arith.compare("complex")) {
+   std::cout<<"Complex matrix; use zreadMM instead!" << std::endl;
+   return false;
+  }
+  else if(!arith.compare("pattern")) {
+   std::cout<<"Pattern matrix; values are needed!" << std::endl;
+   return false;
+  }
+  else {
+   std::cout<<"Unknown arithmetic" << std::endl;
+   return false;
+  }
+ }
+ while (!line.compare(0,1,"%"))
+ {
+  std::getline(inFile, line);
+ }
+ std::istringstream issDim(line);
+ if (!(issDim >> n >> n >> NNZ)){
+  std::cout<<"The matrix dimension is missing" << std::endl;
+  return false;
+ }
+ if(n <= 0 || NNZ <= 0)
+  return false;
+ col = new int[n + 1]();
+ // colL = new int[n + 1]; colU = new int[n + 1];
+ row = new int[NNZ];
+ // rowL = new int[factorSize]; rowU = new int[factorSize];
+ val = new double[NNZ];
+ // valL = new double[factorSize]; valU = new double[factorSize];
+ if(!val || !col || !row)
+  return false;
+ //Initializing the result vector
+ int y, x, colCnt=0, nnzCnt=0;
+ double value;
+
+ col[0]=0;
+ for (int i = 0; i < NNZ; ++i) {
+    inFile>>x;//zero indexing
+    inFile>>y;
+    inFile>>value;
+    if(y > n || y > x || x > n) {
+        std::cout << x << " " << y << " " << value ;
+        return false;
+    }
+    val[i] = value;
+    row[i] = x - 1;
+    ++col[y];
+ }
+ for (int i = 2; i <= n; ++i)
+    col[i] += col[i-1];
+ return true;
+}
+
+bool readRHSMatrix(std::string fName, double* &val, const int size) {
+
+ std::ifstream inFile;
+ inFile.open(fName);
+ std::string line,banner, mtx, crd, arith, sym;
+ /*  File format:
+  *    %%MatrixMarket matrix coordinate real general/symmetric/...
+  *    % ...
+  *    % (optional comments)
+  *    % ...
+  *    #rows    #non-zero
+  *    Triplet in the rest of lines: row    col    value
+  */
+ std::getline(inFile,line);
+ for (unsigned i=0; i<line.length(); line[i]=tolower(line[i]),i++);
+ std::istringstream iss(line);
+ if (!(iss >> banner >> mtx >> crd >> arith >> sym)){
+  std::cout<<"Invalid header (first line does not contain 5 tokens)" << std::endl;
+  return false;
+ }
+
+ if(banner.compare("%%matrixmarket")) {
+  std::cout<<"Invalid header (first token is not \"%%%%MatrixMarket\")" << std::endl;
+  return false;
+ }
+ if(mtx.compare("matrix")) {
+  std::cout<<"Not a matrix; this driver cannot handle that." << std::endl;
+  return false;
+ }
+ if(crd.compare("coordinate")) {
+  std::cout<<"Not in coordinate format; this driver cannot handle that." << std::endl;
+  return false;
+ }
+ if(arith.compare("real")) {
+  if(!arith.compare("complex")) {
+   std::cout<<"Complex matrix; use zreadMM instead!" << std::endl;
+   return false;
+  }
+  else if(!arith.compare("pattern")) {
+   std::cout<<"Pattern matrix; values are needed!" << std::endl;
+   return false;
+  }
+  else {
+   std::cout<<"Unknown arithmetic" << std::endl;
+   return false;
+  }
+ }
+ while (!line.compare(0,1,"%"))
+ {
+  std::getline(inFile, line);
+ }
+ int n, cols, NNZ;
+ std::istringstream issDim(line);
+ if (!(issDim >> n >> cols >> NNZ)){
+  std::cout<<"The matrix dimension is missing" << std::endl;
+  return false;
+ }
+ if(n <= 0 || NNZ <= 0)
+  return false;
+ if (n != size) {
+     std::cout << "Dimensions of L and RHS do not match" << std::endl;
+     std::cout << size << " " << n << std::endl;
+     return false;
+ }
+ if (cols != 1) {
+     std::cout << "RHS matrix has incorrect dimensions" << std::endl;
+     return false;
+ }
+ val = new double[n];
+ // valL = new double[factorSize]; valU = new double[factorSize];
+ if(!val)
+  return false;
+ //Initializing the result vector
+ int y, x;
+ double value;
+
+ for (int i = 0; i < NNZ; ++i) {
+    inFile>>x;//zero indexing
+    inFile>>y;
+    inFile>>value;
+    if(y > 1 || x > n) {
+        std::cout << x << " " << y << " " << value ;
+        return false;
+    }
+    val[x-1] = value;
+ }
+ return true;
+}
